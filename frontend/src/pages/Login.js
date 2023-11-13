b2cdab158c9b039408f9ba518f29a3c07925679d
samba2001 <reddy200101@gmail.com>
2023-11-13 18:16:39 +0530

    added login and register components

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginComp from '../components/login.component';
+import Register from '../components/register.component';
+import { Radio } from 'antd';
 
 const Login = () => {
+  const [Login,setLogin] = useState(true)
   return (
-    <div>Login</div>
-  )
+<div className='login-card'>
+  <div className='row'>
+  <Radio.Group options={['Login','Register']}
+   value={Login ? 'Login' : 'Register'}
+  onChange={()=>{
+    setLogin(!Login)
+  }}  optionType="button" />
+
+  </div>
+  <div> {
+    Login ?  <LoginComp></LoginComp> :<Register></Register>}
+
+  </div>
+
+</div>  )
 }
 
 export default Login;
