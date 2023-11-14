77c3f2f64f97040abf6957d22d3502edfd8715db
sairamyadhav <sairamyadhav4@gmail.com>
2023-11-14 11:35:00 +0530

    extended auth

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import MyTokenObtainPairView
+from .views import MyTokenObtainPairView, Register
 
 urlpatterns = [
     path('login', MyTokenObtainPairView.as_view(), name='login'),
-    # path('register', Register.as_view(), name='register'),
+    path('register', Register.as_view(), name='register'),
 ]
--- backend/user_management/views.py
+++ backend/user_management/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
 from rest_framework.views import APIView
+from base.exceptions import FormDataNotFoundException, FormDataValidationException
 
 # Create your views here.
 
@@ -20,8 +24,30 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-# class Register(APIView):
-#     permission_classes = (AllowAny,)
-#
-#     @staticmethod
-#     def post(request):
+class Register(APIView):
+    permission_classes = (AllowAny,)
+
+    @staticmethod
+    def post(request):
+        try:
+            if not (request.data['username'] and request.data['password'] and request.data['email']):
+                raise FormDataNotFoundException('Required Fields Missing')
+            username, email, password = request.data['username'], request.data['email'], request.data['password']
+            if User.objects.filter(username=username).exists():
+                raise FormDataValidationException('This username is already taken')
+            if User.objects.filter(email=email).exists():
+                raise FormDataValidationException('This email is already taken')
+            user = User.objects.create_user(username, email, password, is_superuser=request.data.get('is_superuser', False))
+            if user:
+                token = MyTokenObtainPairSerializer.get_token(user)
+                return Response({'response': "Success", "message": "user successfully created", "data": token},
+                                status=status.HTTP_200_OK)
+        except FormDataNotFoundException as error:
+            return Response({"response": "Failed", "message": error},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except FormDataValidationException as error:
+            return Response({'response': "Failed", "message": error},
+                            status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'response': "Failure", 'message': str(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
