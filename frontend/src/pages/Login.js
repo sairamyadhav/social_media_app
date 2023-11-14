abb464081ce332efa86a5ef2074d8d9fa17a8d12
samba2001 <reddy200101@gmail.com>
2023-11-14 08:47:40 +0530

    changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,23 +4,23 @@ import Register from '../components/register.component';
 import { Radio } from 'antd';
 
 const Login = () => {
-  const [Login,setLogin] = useState(true)
+  const [Login, setLogin] = useState(true)
   return (
-<div className='login-card'>
-  <div className='row'>
-  <Radio.Group options={['Login','Register']}
-   value={Login ? 'Login' : 'Register'}
-  onChange={()=>{
-    setLogin(!Login)
-  }}  optionType="button" />
+    <div className='login-card'>
+      <div className='row'>
+        <Radio.Group options={['Login', 'Register']}
+          value={Login ? 'Login' : 'Register'}
+          onChange={() => {
+            setLogin(!Login)
+          }} optionType="button" />
 
-  </div>
-  <div> {
-    Login ?  <LoginComp></LoginComp> :<Register></Register>}
+      </div>
+      <div> {
+        Login ? <LoginComp /> : <Register />}
 
-  </div>
+      </div>
 
-</div>  )
+    </div>)
 }
 
 export default Login;
